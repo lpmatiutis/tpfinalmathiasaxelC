9b884866d5e2b83b64b1eac1e9b595bdb7b422be
Mathias Gonzalez <lpthemaster@hotmail.com>
2019-12-07 21:36:55 -0300

    primer intento de juego

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct CARTA
+{
+	int valor, grupo, estado;
+	char color, palo;
+	char impresion[4];
+}Carta;
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
